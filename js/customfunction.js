39c59f6858206e997286102d8519044c9f17aa42
219003113 <42488276+219003113@users.noreply.github.com>
2019-10-24 16:53:57 +0530

    next

--- js/customfunction.js
+++ js/customfunction.js
@@ -3,3 +3,14 @@ target_element.addEventListener( "click", function()
     {
         $zoho.salesiq.visitor.customaction("Trigger");
     });
+
+$zoho.salesiq.ready=function(embedinfo)
+{
+   $zoho.salesiq.visitor.customaction("Track-Signup-button");
+   $zoho.salesiq.visitor.info({"Band" : "Premium", "Branch": "Delhi"});
+   $zoho.salesiq.custom.html("zsiq_float", { 
+	"online.html" :"<img src = 'https://www.zoho.com/salesiq/img/Zilliumonline.png' >", 
+	"offline.html" :"<img src = 'https://www.zoho.com/salesiq/img/ZilliumOffline.png' >",
+	"online.click" :function(){$zoho.salesiq.visitor.info({"CUSTOM" : "FINISHED", "ACTION": "DONE"});}		
+   });
+}
